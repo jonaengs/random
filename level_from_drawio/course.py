c5024a5da25297e4b9bde98591f5cde4e6ae0b1d
jonatan <jonaengs@gmail.com>
2021-03-09 15:05:57 +0100

    switch from tkinter to pygame. Change map

--- level_from_drawio/course.py
+++ level_from_drawio/course.py
@@ -1,51 +1,52 @@
-from tkinter import Tk, Canvas, Frame, BOTH
 from dataclasses import dataclass, fields
+from operator import attrgetter
 
+import pygame
+from time import sleep
 
 @dataclass
 class Element:
-    style: str
+    shape: str
     width: int
     height: int
+    function: str = None
+    rotation: int = 0
     x: int = 0
     y: int = 0
 
-    def get_shape(self):
-        return self.x, self.y, self.width, self.height
+    @staticmethod
+    def field_names():
+        return list(map(attrgetter("name"), fields(Element)))
 
-    def get_shifted(self, course_width, course_height):
-        return course_width - self.x, course_height - self.y, self.width, self.height
-
+    def as_rect(self):
+        return pygame.Rect(self.x, self.y, self.width, self.height)
 
 class Course:
-    required = ["spawn", "hole", "width", "height"]
+    required_elems = ["spawn", "hole"]
 
-    @staticmethod
-    def get_shape_method(canvas, style):
-        return {
-            "rectangle": canvas.create_rectangle,
-            "elipse": canvas.create_oval
-        }.get(style, lambda *x, **y: x)
-
-    
     def __init__(self):
         self.elements = []
 
     def validate(self):
-        assert all(hasattr(self, attr) for attr in Course.required)
+        assert type(self.width) == type(self.height) == int
+        assert self.width > 0 and self.height > 0
+        assert all(any(e.function == attr for e in self.elements) for attr in Course.required_elems)
 
     def show(self):
-        # https://zetcode.com/tkinter/drawing/
-        frame = Frame()
-        frame.pack(fill=BOTH, expand=1)
-        canvas = Canvas(frame, width=self.width, height=self.height)
-        for elem in self.elements:
-            self.get_shape_method(canvas, elem.style)(*elem.get_shifted(self.width, self.height), fill="red")
-
-        canvas.pack(fill=BOTH, expand=1)
-        tk = Tk()
-        tk.mainloop()
+        def draw_triangle(screen, color, rect):
+            points = [(rect.x, rect.y), (rect.x + rect.width, rect.y), (rect.x + rect.width/2, rect.y + rect.height)]
+            shape = pygame.draw.polygon(screen, color, points)
+            return shape
             
+        pygame.init()
+        screen = pygame.display.set_mode((self.width, self.height))
+        screen.fill(pygame.Color("white"))
 
-
-
+        draw_funcs = {"rectangle": pygame.draw.rect, "ellipse": pygame.draw.ellipse, "triangle": draw_triangle}
+        for e in self.elements:
+            draw_funcs.get(e.shape, lambda *x: print("invalid shape: ", e.shape))(
+                screen, pygame.Color("black"), e.as_rect()
+            )
+        
+        pygame.display.flip()
+        sleep(5)  
--- level_from_drawio/parse.py
+++ level_from_drawio/parse.py
@@ -1,38 +1,40 @@
 import xml.etree.ElementTree as ET
 from dataclasses import fields
-from operator import attrgetter
 from course import Course, Element
+from pprint import pprint
 
 # https://docs.python.org/3/library/xml.etree.elementtree.html#module-xml.etree.ElementTree
 
-course = Course()
+def get_styles(cell):
+    styles = cell.get("style").split(";")
+    return {
+        "shape": styles[0] if "=" not in styles[0] else "rectangle",
+        "rotation": next((int(s.split("=")[1]) for s in styles if s.startswith("rotation")), 0)
+    }
 
-tree = ET.parse("drawio.xml")
-root = tree.getroot()[0][0][0]
+def parse(file):
+    course = Course()
+    tree = ET.parse(file)
+    root = tree.getroot()[0][0][0]
 
-def get_style(cell):
-    style = cell.get("style").split(";")[0]
-    return style if "=" not in style else "rectangle"
+    for cell in root:
+        if not cell:  # cell has no children
+            continue
 
-for cell in root:
-    try:
         geometry = cell[0]
-    except IndexError:
-        continue
-    if cell.get("value") == "COURSE":
-        course.width = int(geometry.get("width"))
-        course.height = int(geometry.get("height"))
-    else:
-        elem = Element(
-            **{k: int(v) for k, v in geometry.attrib.items() if k in list(map(attrgetter("name"), fields(Element)))},
-            style=get_style(cell)
-        )
-        course.elements.append(elem)
-        if cell.get("value") == "SPAWN":
-            course.spawn = elem
-        elif cell.get("value") == "HOLE":
-            course.hole = elem
-        
-print(course.__dict__)
-course.validate()
-course.show()
+        if cell.get("value") == "COURSE":
+            course.width, course.height = int(geometry.get("width")), int(geometry.get("height"))
+        else:
+            elem = Element(
+                **{k: int(v) for k, v in geometry.attrib.items() if k in Element.field_names()},
+                **get_styles(cell),
+                function=cell.get("value").lower()
+            )
+            course.elements.append(elem)
+
+    pprint(course.__dict__)
+    course.validate()        
+    return course
+    
+if __name__ == '__main__':
+    parse("drawio.xml").show()
